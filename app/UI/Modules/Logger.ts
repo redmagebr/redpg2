7ec4cb519b3feddd0490393c30519fb149590d38
Reddo <redmagebr@hotmail.com>
2016-10-18 20:58:51 -0200

    updateBeta

--- app/UI/Modules/Logger.ts
+++ app/UI/Modules/Logger.ts
@@ -194,16 +194,14 @@ module UI.Logger {
 
     export function saveLog () {
         // //LOGGERTARGET
-        //var log = currentRoom.exportAsLog(filter());
+        var log = currentRoom.exportAsLog(filter());
 
-        //html = html.replace("//LOGGERTARGET", "UI.Logger.openLog(" + JSON.stringify(log) + ");");
+        html = html.replace("//LOGGERTARGET", "UI.Logger.openLog(" + JSON.stringify(log) + ");");
 
         html = html.replace("href='images/favicon.ico'", "href='" + Server.CLIENT_URL + "images/favicon.ico'")
-            .replace("src='js/lib", "src='" + Server.CLIENT_URL + "js/lib");
-
-        html = html.replace("<link href='stylesheets/screen.css' media='all' rel='stylesheet' type='text/css'>", "<link href='" + Server.CLIENT_URL + "stylesheets/screen.css' media='all' rel='stylesheet' type='text/css'>")
-
-        html = html.replace("<script src='js/Application.js' type='text/javascript'></script>", "<script type='text/javascript'>\n" + js + "\n</script>");
+                   .replace("src='js/lib", "src='" + Server.CLIENT_URL + "js/lib")
+                   .replace("<link href='stylesheets/screen.css' media='all' rel='stylesheet' type='text/css'>", "<link href='" + Server.CLIENT_URL + "stylesheets/screen.css' media='all' rel='stylesheet' type='text/css'>")
+                   .replace("<script src='js/Application.js' type='text/javascript'></script>", "<script type='text/javascript'>\n" + js + "\n</script>");
 
 
         var blob = new Blob([html], {
